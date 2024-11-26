db31d234b90741912c9a3fa74fa132162e4b7b04
“Mateja <mateja.mitic22@gmail.com>
2024-11-26 14:24:28 +0100

    projectInit

---
+++ Cocktails9/Cocktails9/Cocktails9App.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktails9App.swift
+//  Cocktails9
+//
+//  Created by Mateja Mitic on 26.11.24..
+//
+
+import SwiftUI
+
+@main
+struct Cocktails9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cocktails9/Cocktails9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cocktails9
+//
+//  Created by Mateja Mitic on 26.11.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
